66ec8327861dc66b527983e9d633584e4a02416a
Dan <dth26@pitt.edu>
2015-08-05 18:34:43 +0000

    implement yelp url; add Charlotte changes

--- models/modelsDestinations.py
+++ models/modelsDestinations.py
@@ -6,9 +6,23 @@ from flask import request, url_for, render_template, redirect, session, jsonify
 from app import app
 import sys
 import os
-
+import urllib2
+import urllib
+import oauth2
+import json
 
 basedir = "/home/tripplanner/mysite"
+
+
+YELP_HOST = 'api.yelp.com'
+YELP_PATH = '/v2/search/'
+
+
+# OAuth credential
+CONSUMER_KEY = 's2ndfBQeDdPb3yz6JQpeRA'
+CONSUMER_SECRET = 'XCiSSKmUcjwzCF2bTeqWdAWWEDw'
+TOKEN = 'ZpNcuOUbNo48E9XFqL3eWZIF6i9IG0Uo'
+TOKEN_SECRET = 'wtCI1TK7RBET0GTN2pL5Xgb-4Jc'
 
 
 class Destinations(db.Model):
@@ -22,10 +36,12 @@ class Destinations(db.Model):
 	address = Column(String)
 	description = Column(String)
 	url = Column(String)
+	yelp_url = Column(String)
 	imgPath = Column(String)
 	visited = Column(Integer)
+	category = Column(String)
 
-	def __init__(self, ID, name, latitude, longitude, address, description, url, imgPath, visited):
+	def __init__(self, ID, name, latitude, longitude, address, description, url, yelp_url, imgPath, visited, category):
 		self.ID = ID
 		self.name = name
 		self.latitude = latitude
@@ -35,6 +51,8 @@ class Destinations(db.Model):
 		self.url = url
 		self.imgPath = imgPath
 		self.visited = visited
+		self.yelp_url = yelp_url
+		self.category = category
 
 db.create_all()
 
@@ -59,6 +77,8 @@ def getDestinations():
         newRow['visited'] = row['visited']
         newRow['latitude'] = row['latitude']
         newRow['longitude'] = row['longitude']
+        newRow['yelp_url'] = row['yelp_url']
+        newRow['category'] = row['category']
         dictionary.append(newRow)
 
     return jsonify(list=dictionary)
@@ -72,12 +92,22 @@ def addNewDestination():
     name = request.args.get('Name', type=str)
     latitude = request.args.get('Latitude', type=float)
     longitude = request.args.get('Longitude', type=float)
-    #latitude = float(latitudeStr)
-    #longitude = float(longitudeStr)
+    category = request.args.get('category', type=str)
+    yelp_url = None
+
+    if 'yelp.com' in URL:
+        yelp_url = URL
+    else:
+        json = getYelpInfo(name, address)
+        try:
+            yelp_url = json['businesses'][0]['mobile_url']
+        except:
+            yelp_url = 'no result!'
+
 
     engine = create_engine('sqlite:///' + os.path.join(basedir, 'db_file.db'), echo=True)
     connection = engine.connect()
-    query = text('INSERT INTO Destinations(`address`,`description`,`name`,`url`,`longitude`,`latitude`) VALUES(:address, :description, :name, :url, :longitude, :latitude)')
+    query = text('INSERT INTO Destinations(`address`,`description`,`name`,`url`,`longitude`,`latitude`, `yelp_url`, `category`) VALUES(:address, :description, :name, :url, :longitude, :latitude, :yelp_url, :category)')
     connection.execute(
                   query,
                   address = address,
@@ -85,7 +115,9 @@ def addNewDestination():
                   name = name,
                   longitude = longitude,
                   latitude = latitude,
-                  url = URL)
+                  url = URL,
+                  yelp_url = yelp_url,
+                  category = category)
     connection.close()
 
     return jsonify(name=name)
@@ -102,4 +134,47 @@ def deleteDestination():
 	connection.close()
 
 	return jsonify(success=True)
+
+
+
+# ex. Dinosaurs|37.7646602|-122.4321068
+# get yelp json data
+def getYelpInfo(name, address):
+    response = None
+    request_url = 'http://{0}{1}?'.format(YELP_HOST, urllib.quote(YELP_PATH.encode('utf8')))
+
+    url_params = {
+            'location':address
+        }
+
+    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+    oauth_request = oauth2.Request(method="GET", url=request_url, parameters=url_params)
+    oauth_request.update(
+        {
+            'oauth_nonce': oauth2.generate_nonce(),
+            'oauth_timestamp': oauth2.generate_timestamp(),
+            'oauth_token': TOKEN,
+            'oauth_consumer_key': CONSUMER_KEY
+        }
+    )
+    token = oauth2.Token(TOKEN, TOKEN_SECRET)
+    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+    signed_url = oauth_request.to_url()
+    conn = urllib2.urlopen(signed_url, None)
+
+    try:
+        response = json.loads(conn.read())
+    except Exception as inst:
+        response = {'error':inst}
+    finally:
+        conn.close()
+
+    return response
+
+
+
+
+
+
+
 
