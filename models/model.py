eb26765b1e738f6e4bea86b43c01b8999663b789
dth26 <dth26@pitt.edu>
2015-09-28 22:55:33 -0400

    Update model.py
    fix login

--- models/model.py
+++ models/model.py
@@ -3,7 +3,6 @@ from modelsDirections import *
 from yelpScraper import *
 
 from __init__ import db
-from passlib.apps import custom_app_context as pwd_context
 from sqlalchemy import *
 from sqlalchemy.orm import relationship, backref
 from flask import request, url_for, render_template, redirect, session
@@ -82,9 +81,4 @@ def logout():
 def isLogged():
     return jsonify(session);
 
-
-# set the secret key.  keep this really secret:
-# create a session
-# any script that uses session must add this line of code
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
